a17dec16f6b813c680bdc849e84ba63683db2782
Josimar Silva <josimar.silva@me.com.br>
2021-08-20 12:13:37 -0300

    Start project with web and thymeleaf

---
+++ src/test/java/com/josimas/jokes/JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josimas.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
